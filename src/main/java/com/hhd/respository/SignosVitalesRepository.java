bdd1ed37f105fdda1d6b68b490caf22a98c15ff7
edarancibia <edarancibia11@gmail.com>
2020-11-28 21:34:03 -0300

    glasgow2

--- src/main/java/com/hhd/respository/SignosVitalesRepository.java
+++ src/main/java/com/hhd/respository/SignosVitalesRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SignosVitalesRepository extends JpaRepository<SignosVitales, Long> {
      
-     @Query(value = "select DATE_FORMAT(s.fecha,'%d-%m-%Y %H:%i') as fecha,fc,sat,pa,tax,hgt,resp \r\n"
+     @Query(value = "select DATE_FORMAT(s.fecha,'%d-%m-%Y %H:%i') as fecha,fc,sat,pa,tax,hgt,resp, glasgow \r\n"
      		+ "from signos_vitales s \r\n"
      		+ "where s.id_ficha = :idFicha order by s.fecha  desc", nativeQuery = true)
      List<Map<String, Object>> findSignosVitalesByIdFicha(Long idFicha);
